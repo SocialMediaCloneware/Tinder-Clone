9b9bdf530b543ee7c98d1bd59c4e6aaa7e541b7c
Daniel Hong <hongchangyeon@gmail.com>
2020-12-13 15:02:07 -0600

    added forum button

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Header.css";
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
     return (
@@ -16,8 +17,12 @@ function Header() {
             src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
             alt="tinder logo"
         />
-
-            
+        
+        <IconButton> 
+        <ForumIcon fontSize='large' className="header__icon"/>
+        </IconButton>
+        
+         
         </div>
 
 
