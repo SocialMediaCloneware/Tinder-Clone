16e450dcde22fd0f4d97db9f9f61c76313f11934
Daniel Hong <hongchangyeon@gmail.com>
2020-12-13 14:28:59 -0600

    npm i @material-ui/icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-     
-      
     <Header/>
     {/* TinderCards */}
     {/* SwipeButtons */}
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import "Header.CSS";
+import "./Header.css";
+import PersonIcon from '@material-ui/icons/Person';
 
 function Header() {
     return (
         <div className='header'>
             <h2>I am header</h2>
+            <PersonIcon/>
         </div>
     )
 }
