eddcb6b151b675c45f08d68b30322b22b0d6af98
Daniel Hong <hongchangyeon@gmail.com>
2020-12-13 15:45:50 -0600

    react state and hook

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,9 +1,23 @@
+import { PeopleRounded } from '@material-ui/icons';
 import React from 'react'
+import "./TinderCards.css"
 
 function TinderCards() {
+    const [people, setPeople] = useState([
+        {
+            name: 'Elon Musk',
+            url: "https://m.media=amazon.com/images/M/MV5BMTI5NDY5NjU3NF5BMl5BanBnXkFtZTcwMzQ0MTMyMw@@._V1_UY1200_CR94,0,630,1200_AL_.jpg",
+        }
+    ]);
+    
+
     return (
-        <div>
-            
+        <div className="tinderCards">
+            {people.map(person =>(
+            <h1>{person.name}</h1>  
+            ))}
+
+
         </div>
     )
 }
