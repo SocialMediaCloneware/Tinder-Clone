99a5a4a5c588c3d6fe18c7711559abf3d6c6edcb
Daniel Hong <hongchangyeon@gmail.com>
2020-12-13 19:46:03 -0600

    added useEffect

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,19 +1,17 @@
-import React, {useState} from 'react'
+import axios from 'axios';
+import React, {useState, useEffect} from 'react'
 import TinderCard from 'react-tinder-card'
 import "./TinderCards.css"
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-            name: 'Jeff Bezos',
-            url: "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5bb22ae84bbe6f67d2e82e05%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D560%26cropX2%3D1783%26cropY1%3D231%26cropY2%3D1455",
-        },
-        {
-            name: 'Elon Musk',
-            url: "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f47d4de7637290765bce495%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1398%26cropX2%3D3908%26cropY1%3D594%26cropY2%3D3102",
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+         const req = await axios.get("/tinder/cards")
+         setPeople(req.data);
         }
-        
-    ]);
+    }, [])
     
     const swiped = (direction, nameToDelete) => {
         console.log("removing: " + nameToDelete);
