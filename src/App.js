3b34a98e2b0c765b3db6977bf21eff0815e0d2f5
Daniel Hong <hongchangyeon@gmail.com>
2020-12-14 23:12:36 -0600

    chatscreen component added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import Header from "./Header";
 import TinderCards from "./TinderCards";
 import SwipeButtons from "./SwipeButtons";
 import Chats from './Chats';
+import ChatScreen from './ChatScreen';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
 
-    {/* <Router>      */}
+    {/* <Router>  */}
     <Router>  
     <Switch>
 
+    <Route path="/chat/:person">
+
+    {/* Back Button */}
+    <Header backButton="/chat" />
+    
+    {/* Chatscreen component */}
+    <ChatScreen />
+    </Route>
+
     <Route path="/chat">
+
     {/* Back Button */}
     <Header backButton="/" />
     
